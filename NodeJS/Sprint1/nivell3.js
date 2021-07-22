5b5ef5a12104843ae9cfeceaa0308cd5eabad8e8
TheKving <kevingomezhurtado@gmail.com>
2021-07-22 11:49:15 +0200

    Add files via upload

---
+++ NodeJS/Sprint1/nivell3.js
@@ -0,0 +1,15 @@
+/* EXE1 */
+
+
+
+/*Exe2 */
+let name = "Kevin";
+
+var nameAnonFunc = (function(aname) { 
+    var printname = 'Anonymous Function Name is '+aname; 
+    console.log(printname);
+})(name);
+
+
+
+
